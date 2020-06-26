739b5d345e903ecd4041d343d1ab7da3ef65fc4a
Edward Cochra <ecochra@umich.edu>
2020-06-26 19:54:51 -0400

    Added a bit more code

--- js/template.js
+++ js/template.js
@@ -1,7 +1,27 @@
 console.log("Sample Trello PowerUp -- Hello World");
 
-// TrelloPowerUp.initialize({
-//     "card-badges": function(t,opts) {
-//         return [];
-//     }
-// });
+var GRAY_ICON = "https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg";
+
+var onBtnClick = function(t,opts) {
+    console.log("Click");
+}
+
+TrelloPowerUp.initialize({
+    "card-badges": function(t,opts) {
+        return [];
+    },
+    "card-buttons": function(t,opts) {
+        return [{
+            icon: GRAY_ICON,
+            text: "Sample Button",
+            callback: onBtnClick,
+            condition: "edit"
+        }, {
+            icon: GRAY_ICON,
+            text: "Just a URL",
+            condition: "always",
+            url: "https://developer.atlassian.com/cloud/trello",
+            target: "Trello Developer Site"
+        }];
+    }
+});
