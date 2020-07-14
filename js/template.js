8e725baac8f29bc79551ae5924a934794c394167
Edward Cochra <ecochra@umich.edu>
2020-07-14 10:14:51 -0400

    Commented out unused code and switched img path to variable

--- js/template.js
+++ js/template.js
@@ -1,12 +1,12 @@
-console.log("Sample Trello PowerUp -- Hello World");
+console.log("Last Updated Trello PowerUp -- Hello World");
 
-var GRAY_ICON = "https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg";
+var DATE_ICON = "./assets/img/calendar-and-clock.png";
 
-var onBtnClick = function(t,opts) {
-    console.log(t);
-    console.log(opts);
-    console.log("Click");
-}
+// var onBtnClick = function(t,opts) {
+//     console.log(t);
+//     console.log(opts);
+//     console.log("Click");
+// }
 
 TrelloPowerUp.initialize({
     "card-badges": function(t,opts) {
@@ -17,24 +17,24 @@ TrelloPowerUp.initialize({
             return [{
                 text: `Last Updated: ${date.toLocaleDateString()}`,
                 color: "sky",
-                icon: "./assets/img/calendar-and-clock.png"
+                icon: DATE_ICON
             }];
         });
     },
-    "card-buttons": function(t,opts) {
-        return [{
-            icon: GRAY_ICON,
-            text: "Sample Button",
-            callback: onBtnClick,
-            condition: "edit"
-        }, {
-            icon: GRAY_ICON,
-            text: "Just a URL",
-            condition: "always",
-            url: "https://developer.atlassian.com/cloud/trello",
-            target: "Trello Developer Site"
-        }];
-    }
+    // "card-buttons": function(t,opts) {
+    //     return [{
+    //         icon: GRAY_ICON,
+    //         text: "Sample Button",
+    //         callback: onBtnClick,
+    //         condition: "edit"
+    //     }, {
+    //         icon: GRAY_ICON,
+    //         text: "Just a URL",
+    //         condition: "always",
+    //         url: "https://developer.atlassian.com/cloud/trello",
+    //         target: "Trello Developer Site"
+    //     }];
+    // }
 }),{
     appKey: ""
 };
